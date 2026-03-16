8bea176686346d8c483a72223771dd0b9c77fbed
khobaya8-eng <khobaya8@gmail.com>
2026-03-16 16:31:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,32 @@ async function loadFlights() {
 
 let result = document.getElementById("result");
 
+result.innerHTML = "Loading flights...";
+
 try {
 
 let response = await fetch("https://opensky-network.org/api/states/all");
 let data = await response.json();
 
-let flights = data.states.slice(0,10); // show first 10 flights
+let flights = data.states.slice(0,10);
 
 let html = "<h3>Live Flights</h3>";
 
 flights.forEach(flight => {
+
 html += `
 Flight: ${flight[1]} <br>
 Country: ${flight[2]} <br>
 Altitude: ${flight[7]} meters <br><br>
 `;
+
 });
 
 result.innerHTML = html;
 
-} catch(error) {
+}
+
+catch(error){
 
 result.innerHTML = "Error loading flight data.";
 
